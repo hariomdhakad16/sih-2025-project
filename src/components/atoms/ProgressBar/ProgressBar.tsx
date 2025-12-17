516252af97e83c516670b3d3ddee011dd38a9b19
Rajkumar <mavairk@gmail.com>
2025-12-17 16:47:17 +0530

    add ProgressBar component and corresponding Storybook stories

---
+++ src/components/atoms/ProgressBar/ProgressBar.tsx
@@ -0,0 +1,11 @@
+const ProgressBar =() => {
+    return (
+
+        <label>
+            Uploading Document: <progress value="70" max="100">70 %</progress>
+        </label>
+
+
+)
+}
+export default ProgressBar;
